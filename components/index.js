ab34e6f55115390c6c4e31e76c58d78ac4e1b7a9
IvanZharnov <ivan.zharnov@gmail.com>
2019-07-17 15:20:58 +0300

    add levels logic

--- components/index.js
+++ components/index.js
@@ -3,7 +3,18 @@ import { createPerson } from './person.js'
 import { createShuriken } from './shuriken.js'
 import { createStopwatch } from './stopwatch.js'
 
+let interval = 5000;
+
+const throwShuriken = () => {
+  setTimeout(() => throwShuriken(), interval);
+  createShuriken(app)
+}
+
+setInterval(() => {
+  return interval <= 1000 ? interval = 1000 : interval = interval - 1000
+}, 20001)
+
 const app = createFrame();
 createPerson(app);
-setInterval(() => createShuriken(app), 3000);
-createStopwatch()
+createStopwatch();
+setTimeout(() => throwShuriken(), 3000)
